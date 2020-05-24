9734eaa1318ace5209715b60854554b953f87810
Jose <quirroone@gmail.com>
2020-05-24 10:58:16 -0500

    Add get todos function

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,11 +1,22 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { parseUserId } from '../../auth/utils'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+const indexName = process.env.INDEX_NAME
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+  const userId = parseUserId(jwtToken)
+
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+
 
   // TODO: Implement creating a new TODO item
   return undefined
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,44 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+import { parseUserId } from '../../auth/utils'
+
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+const indexName = process.env.INDEX_NAME
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+  const userId = parseUserId(jwtToken)
+
+  const items = getTodosForUser(userId)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(items)
+  }
+}
+
+async function getTodosForUser(userId: string){
+  const result = await docClient.query({
+    TableName: todosTable,
+    IndexName: indexName,
+    KeyConditionExpression: 'userId= :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    }
+  }).promise()
+
+  console.log(result)
+
+  return result
 }
