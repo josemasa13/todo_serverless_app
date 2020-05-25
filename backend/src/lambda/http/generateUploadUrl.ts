eaa0b8369dee7b0472628da8bc09b26302d12fa5
Jose <quirroone@gmail.com>
2020-05-25 15:34:36 -0500

    Change attachment id for todo items so it can be changed

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import { setAttachmentUrl } from "../../Logic/todos";
 import { createLogger } from '../../utils/logger';
+import * as uuid from 'uuid'
 
 const logger = createLogger('uploadTodoUrl');
 
@@ -19,7 +20,8 @@ const urlExpiration = process.env.SIGNED_URL_EXPIRATION
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     const url = getUploadUrl(todoId)
-    const attachmentUrl = `https://${bucketName}.s3.amazonaws.com/${todoId}`
+    const attachmentId = uuid.v4()
+    const attachmentUrl = `https://${bucketName}.s3.amazonaws.com/${attachmentId}`
     await setAttachmentUrl(todoId, attachmentUrl)
     logger.info(`attaching url to the ${todoId} todo item`)
 
